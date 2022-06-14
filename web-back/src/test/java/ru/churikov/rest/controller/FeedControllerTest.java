a2ebacf840a24994e04016f6db5b6b0a49c14750
VVonneX <78104695+VVonneX@users.noreply.github.com>
2022-06-14 15:37:27 +0300

    Обновил бэк

---
+++ web-back/src/test/java/ru/churikov/rest/controller/FeedControllerTest.java
@@ -0,0 +1,112 @@
+package ru.churikov.rest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.churikov.rest.controllers.FeedController;
+import ru.churikov.rest.models.Feed;
+import ru.churikov.rest.models.FeedDto;
+import ru.churikov.rest.service.FeedService;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@ContextConfiguration(classes = {FeedController.class})
+@WebMvcTest
+@WithMockUser
+public class FeedControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FeedService feedService;
+
+    private static final String ID = "1";
+    private static final String NAME = "name";
+    private static final String DESCRIPTION = "description";
+    private static final String URL = "/api/feed";
+
+    @Test
+    void getAllFeed() throws Exception {
+        Mockito.when(feedService.getAllFeed()).thenReturn(getListFeed());
+
+        mockMvc.perform(get(URL + "/all"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", equalTo(NAME)))
+                .andReturn();
+    }
+
+    @Test
+    void getOneByFeed() throws Exception {
+        Mockito.when(feedService.getOntById(Mockito.any())).thenReturn(getFeed());
+
+        mockMvc.perform(get(URL + "/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(NAME)))
+                .andReturn();
+    }
+
+    @Test
+    void createOrEdit() throws Exception {
+        Mockito.when(feedService.save(Mockito.any(Feed.class))).thenReturn(getFeed());
+
+        mockMvc.perform(post(URL).content(readFileAsString("feed/saveFeed.json")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(NAME)))
+                .andReturn();
+    }
+
+    public static String readFileAsString(String filename) throws IOException {
+        File file = new File("src/test/resources/" + filename);
+        Path path = file.toPath();
+        byte[] fileBytes = Files.readAllBytes(path);
+        return new String(fileBytes, StandardCharsets.UTF_8);
+    }
+
+    FeedDto getFeedDto() {
+        FeedDto dto = new FeedDto();
+        dto.setId(Long.parseLong(ID));
+        dto.setName(NAME);
+        dto.setDescription(DESCRIPTION);
+        return dto;
+    }
+
+    List<Feed> getListFeed() {
+        Feed feed = new Feed();
+        feed.setId(Long.parseLong(ID));
+        feed.setName(NAME);
+        feed.setDescription(DESCRIPTION);
+        return Collections.singletonList(feed);
+    }
+
+    Feed getFeed() {
+        Feed feed = new Feed();
+        feed.setId(Long.parseLong(ID));
+        feed.setName(NAME);
+        feed.setDescription(DESCRIPTION);
+        return feed;
+    }
+}
--- web-back/src/test/java/ru/churikov/rest/service/FeedServiceTest.java
+++ web-back/src/test/java/ru/churikov/rest/service/FeedServiceTest.java
@@ -3,60 +3,84 @@ package ru.churikov.rest.service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.churikov.rest.models.Feed;
 import ru.churikov.rest.models.FeedDto;
 import ru.churikov.rest.repoitory.FeedRepository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@ContextConfiguration(classes = {FeedService.class})
 class FeedServiceTest {
-    FeedService service;
-    Feed feed;
-    FeedDto dto;
-    FeedRepository repository = Mockito.mock(FeedRepository.class);
 
-    @BeforeEach
-    public void setUp() {
-        feed  = new Feed("name", "description");
-        dto = new FeedDto();
-        dto.setName("name");
-        dto.setDescription("description");
-        service = new FeedService(repository);
-       feed = service.save(feed);
-    }
+    @Autowired
+    FeedService service;
+
+    @MockBean
+    FeedRepository repository;
+
+    private static final String ID = "1";
+    private static final String NAME = "name";
+    private static final String DESCRIPTION = "description";
 
     @AfterEach
     public void tearDown() {
         Mockito.clearInvocations(repository);
     }
 
-  /*  @Test
+    @Test
     void testGetById() {
-        Mockito.when(repository.findOneById(feed.getId())).thenReturn(Optional.of(feed));
-        Mockito.when(service.getOntById(feed.getId())).thenReturn(feed);
-        Mockito.verify(repository).findOneById(any());
-
+        Mockito.when(repository.findOneById(Mockito.any())).thenReturn(Optional.of(getFeed()));
+        Feed check = service.getOntById(Long.parseLong(ID));
+        assertEquals(check, getFeed());
+        Mockito.verify(repository, Mockito.times(1)).findOneById(any());
     }
 
     @Test
     void testSave() {
-
-        Mockito.when(repository.save(feed)).thenReturn(feed);
-        Mockito.when(service.save(feed)).thenReturn(feed);
-        Mockito.verify(repository, Mockito.times(1)).save(feed);
+        Mockito.when(repository.save(Mockito.any(Feed.class))).thenReturn(getFeed());
+        Feed check = service.save(getFeed());
+        assertEquals(check, getFeed());
+        Mockito.verify(repository, Mockito.times(1)).save(any());
     }
-*/
+
     @Test
     void verifyThatGetAllFeedsSuccessiful() {
         List<Feed> result = new ArrayList<>();
-        Mockito.when(repository.findAll()).thenReturn(result);
-
-        assertEquals(service.getAllFeed(), result);
+        Mockito.when(repository.findAll()).thenReturn(getListFeed());
+        List<Feed> feeds = service.getAllFeed();
+        assertEquals(feeds, getListFeed());
+        Mockito.verify(repository, Mockito.times(1)).findAll();
     }
+
+    List<Feed> getListFeed() {
+        Feed feed = new Feed();
+        feed.setId(Long.parseLong(ID));
+        feed.setName(NAME);
+        feed.setDescription(DESCRIPTION);
+        return Collections.singletonList(feed);
+    }
+
+    Feed getFeed() {
+        Feed feed = new Feed();
+        feed.setId(Long.parseLong(ID));
+        feed.setName(NAME);
+        feed.setDescription(DESCRIPTION);
+        return feed;
+    }
+
 }
