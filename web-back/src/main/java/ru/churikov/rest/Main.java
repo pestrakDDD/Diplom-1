f6252aef492cd4f97bdda35a91c515a8c1f8d595
VVonneX <78104695+VVonneX@users.noreply.github.com>
2022-06-14 14:45:37 +0300

    Добавление Dockerfile и Docker-compose

--- web-back/src/main/java/ru/churikov/rest/Main.java
+++ web-back/src/main/java/ru/churikov/rest/Main.java
@@ -3,9 +3,6 @@ package ru.churikov.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class Main {
